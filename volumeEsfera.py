68f7acf5bfa13548c733f6718271134cbef70bfd
guibonato <ghbdasi@gmail.com>
2025-02-22 20:51:40 -0300

    Correção simples com comentário

--- volumeEsfera.py
+++ volumeEsfera.py
@@ -5,5 +5,6 @@ print("Calculadora do volume da esfera!")
 raio = float(input("Digite o valor do raio da esfera: "))
 
 volume = (4/3)*math.pi*raio**3
+""" pode ser tambem (4/3)*math.pi*math.pow(raio, 3) """
 
 print ("O volume é",volume)
