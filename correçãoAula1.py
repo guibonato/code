2ce94172ce62fff3b144b9d9e3c117d68c564f9e
guibonato <ghbdasi@gmail.com>
2025-02-22 20:07:16 -0300

    Aula 2 - Lógica de Programação

---
+++ correçãoAula1.py
@@ -0,0 +1,7 @@
+# codigo errado: Print('Informe o seu nome')
+# correção
+
+print('Informe o seu nome')
+
+nome = input()
+print('Nome: ', nome)
---
+++ volumeEsfera.py
@@ -0,0 +1,9 @@
+import math
+
+print("Calculadora do volume da esfera!")
+
+raio = float(input("Digite o valor do raio da esfera: "))
+
+volume = (4/3)*math.pi*raio**3
+
+print ("O volume é",volume)
