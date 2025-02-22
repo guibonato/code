2db88376463de48008c7edef6124ed01d0bcee5c
guibonato <ghbdasi@gmail.com>
2025-02-22 20:17:17 -0300

    Exercicios Aula 2

---
+++ correcao2Aula2.py
@@ -0,0 +1,11 @@
+print('Informe o primeiro valor: ')
+valor1 = int (input())
+print('Informe o segundo valor: ')
+valor2 = int (input())
+
+# soma = valor1+valor2/2 
+
+# correção
+soma = (valor1+valor2)/2
+
+print('Média Aritmética = ', soma)
