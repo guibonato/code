7d85a7a7277bf1e494ba097305c835e8168ac86a
guibonato <ghbdasi@gmail.com>
2025-02-22 22:27:35 -0300

    Arrumando a pasta

---
+++ logica_de_programacao/areaEsfera.py
@@ -0,0 +1,11 @@
+import math
+
+print('Calculadora da área da esfera!')
+
+print('Defina o raio da esfera: ')
+
+r = float(input())
+
+area =  4*math.pi*math.pow(r, 2)
+
+print('A área da esfera é: ', area)
---
+++ logica_de_programacao/potenciasN.py
@@ -0,0 +1,9 @@
+import math
+
+print('Calculadora de potências!')
+
+print('Informe a base das potências: ')
+base = float(input())
+
+
+print("As potências valem: ", int(math.pow(base, 2)), int(math.pow(base, 3)), int(math.pow(base, 4)))
