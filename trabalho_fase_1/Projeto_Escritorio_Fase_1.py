61fd5597a02dcf6e43a53300a62ad2d31eadf005
guibonato <ghbdasi@gmail.com>
2025-02-28 18:39:35 -0300

    Entrega final

--- trabalho_fase_1/Projeto_Escritorio_Fase_1.py
+++ trabalho_fase_1/Projeto_Escritorio_Fase_1.py
@@ -1,3 +1,6 @@
+# Criei listas para facilitar e reduzir as linhas de codigo, dessa forma utilizando os 
+# índices para referenciar os meses, consegui torna-lo mais legível e compacto.
+
 lista_temperaturas = [None, None, None, None, None, None, None, None, None, None, None, None]
 
 meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
@@ -5,6 +8,9 @@ meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
 
 meses_escaldantes = []
 
+
+# Desenvolvi duas funções também para que o processo de armazenamento das temperaturas 
+# fosse mais eficiente, além de também poder referenciar o inteiro com um mês específico.
 def ligar_mes (mes):
     return meses[mes]
 
@@ -13,10 +19,16 @@ def add_temp (mes, temperatura):
     lista_temperaturas[mes-1] = temperatura
     return lista_temperaturas
 
+# Usei de um laço de repetição para que o usuário pudesse informar e alterar as 
+# temperaturas quantas vezes desejasse, sem obrigálo a preencher todos os meses
+
 print('Metereologia - Temperatura')
 resposta = 's'
 
 while resposta == 's':
+
+    # Utilizei do bloco try/except para garantir que fossem informados dados válidos, 
+    # e não haver necessidade de usar de comparadores lógicos, que ocasionaria um código mais extenso.
     try:
         mes = int(input('Informe o mês (1 a 12): '))
         # Garante que seja um mês válido
@@ -37,15 +49,25 @@ while resposta == 's':
     except ValueError:
         print('São admitidos apenas números inteiros para o mês e números reais para a temperatura.')
 
+# Para que o usuario não tivesse a saída forçada do programa, pela caracteristica de o python ser case 
+# sensitive, utilizei o método lower para que o programa aceitasse tanto 's' quanto 'S'.
+
+# Aqui criei as variáveis desejadas, para que o programa retornasse cada uma delas, se houvesse alguma ou 
+# nehuma temperatura informada.
 media = 0
 quentes = 0
 max = None
 min = None
 contador_meses = 0
 
+# Neste bloco, usei o laço de repetição da lista (que deverá ter sofrido alterações), de forma que o 
+# programa identifique somente os meses que possuem temperatura informada
 for mes in lista_temperaturas:
     if mes != None:
         media += mes
+
+        # O metodo index() foi apropriado para que eu criasse a lsita com os meses escaldantes, 
+        # e também, para associar os inteiros com os meses.
         if mes > 33:
             meses_escaldantes.append(ligar_mes(lista_temperaturas.index(mes)))
             quentes += 1
@@ -55,15 +77,20 @@ for mes in lista_temperaturas:
         if min == None or mes < min:
             min = mes
             mes_min = ligar_mes(lista_temperaturas.index(mes))
+            # Aqui fiz um contador para poder fazer a média final das temperaturas informadas
         contador_meses += 1
     else:
         continue
+
+    # Essa foi uma linha de código que fiz para evitar o erro de 'Division by zero', caso o usuário nao informasse nada, 
+    # mesmo que o propósito do programa seja esse, é um sistema de segurança para evitar crashes.
 if contador_meses == 0:
     print('Nenhuma temperatura foi informada.')
     exit() 
 
 media = media / contador_meses
 
+# No final são informados todos os requisitos do projeto, utilziaei o método format para que a saída fosse mais legível e organizada.
 print(f'A média das temperaturas é {media:.2f}°C.')
 if quentes == 1:
     print(f'{quentes} mês com temperatura superiores a 33°C.')
