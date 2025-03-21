fa184ce9c4ed071ffe9373d382746eb2cf489f9e
guibonato <ghbdasi@gmail.com>
2025-03-20 21:26:14 -0300

    me ajudaaaaaa

--- logica_de_programacao/aulas_basicas/aulas_avançadas/mexerEmArquivosExternos.py
+++ logica_de_programacao/aulas_basicas/aulas_avançadas/mexerEmArquivosExternos.py
@@ -1,3 +1,3 @@
 print('Aprendendo como fazer a fase 2 do projeto de logica de programação')
 
-data_qualquer = open('D:\Tecnologo Puc\Lógica de Programação\Anexo_Arquivo_Dados_Projeto_Logica_e_programacao_de_computadores.csv', r)
+data_qualquer = open("D:\Tecnologo Puc\Lógica de Programação\TabelaFase2.csv", 'r')
