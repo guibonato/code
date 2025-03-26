d531f4372ac40a979731360f811b3cda45d68a39
guibonato <ghbdasi@gmail.com>
2025-03-26 18:49:00 -0300

    porra

--- logica_de_programacao/aulas_basicas/aulas_avançadas/mexerEmArquivosExternos.py
+++
@@ -1,7 +0,0 @@
-print('Aprendendo como fazer a fase 2 do projeto de logica de programação')
-
-data_qualquer = open("D:\Tecnologo Puc\Lógica de Programação\TabelaFase2.csv", 'r')
-
-
-
-data_qualquer.close()
