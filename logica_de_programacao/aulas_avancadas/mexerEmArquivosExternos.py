1aa547884e0d8440f998749fdd81cb4b45af867f
guibonato <ghbdasi@gmail.com>
2025-03-27 10:03:59 -0300

    remove arquivos desnecessários e adiciona novos scripts de lógica de programação

---
+++ logica_de_programacao/aulas_avancadas/mexerEmArquivosExternos.py
@@ -0,0 +1,9 @@
+""" w = open('teste.txt', 'w')
+
+print(w.write('Escrevendo no arquivo\n'))
+w.close()
+ """
+
+""" w = open('teste.txt')
+print(w.read())
+w.close() """
--- logica_de_programacao/aulas_avançadas/mexerEmArquivosExternos.py
+++
@@ -1,3 +0,0 @@
-w = open("D:\Tecnologo Puc\code\logica_de_programacao\aulas_avançadas\teste.txt")
-
-print(w.read())
